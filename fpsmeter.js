5887e6e3e6fc4247b20838525346bdca8963b302
David Corvoysier <kaizouman@kaizou.org>
2012-11-15 09:43:37 +0100

    Updated opyright date

--- fpsmeter.js
+++ fpsmeter.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 David Corvoysier http://www.kaizou.org
+// Copyright (c) 2012 David Corvoysier http://www.kaizou.org
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
